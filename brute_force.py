1a35387a6c6650b4d08f6f143addd05d540034c1
vpemfh7 <vpemfh7@gmail.com>
2019-09-30 14:50:46 -0300

    Added a fancy output for each elapsed time to crack a password

--- brute_force.py
+++ brute_force.py
@@ -4,21 +4,37 @@ import hashlib
 import time
 from data import *
 
-characters = string.digits
+characters = string.digits + string.ascii_lowercase
 credentials = 'credentials.txt'
 
-def crack(target, chars,size=4):
+
+def crack(target, chars, size=4):
     cracked = ''
     passwords = []
+    time_elapsed = []
     data = get_credentials(target)
     for j in range(len(data)):
+        t0 = time.time()
         for i in itertools.product(chars, repeat=size):
             print(cracked.join(i), hashlib.md5(cracked.join(i).encode('utf8')).hexdigest())
 
             if hashlib.md5(cracked.join(i).encode('utf8')).hexdigest() == data[j][1]:
                 passwords.append(cracked.join(i))
                 print("Password found >> " + '\'' + cracked.join(i) + '\'')
+                time_elapsed.append(time.time() - t0)
                 break
-    print(passwords)
-    
+
+    txt_format = ''
+    for pw in passwords:
+        txt_format += pw + ', '
+    print(txt_format)
+
+    time_each_pw = ''
+    for _time in time_elapsed:
+        time_each_pw += str(_time) + ' seconds' + ', '
+    print(time_each_pw)
+
+
+init_time = time.time()
 crack(credentials, characters)
+print("Total time elapsed >> " + str(time.time() - init_time))
