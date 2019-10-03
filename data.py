b161d6f878e7e0a3bef22c695eb7c7bdd564b2c3
vpemfh7 <vpemfh7@gmail.com>
2019-10-03 10:08:23 -0300

    Added a function to return the line count in a file

--- data.py
+++ data.py
@@ -1,19 +1,22 @@
 def get_credentials(credential_file):
-    with open(credential_file, 'r') as file:
-        lines = ''
-        line_lst = []
+    try:
+        with open(credential_file, 'r') as file:
+            lines = ''
+            line_lst = []
 
-        # Adds data into a string
-        for line in file.readlines():
-            lines += line
+            # Adds data into a string
+            for line in file.readlines():
+                lines += line
 
-        str_lines = lines.splitlines()  # Responsible to separate the data into lines, resulting a list
+            str_lines = lines.splitlines()  # Responsible to separate the data into lines, resulting a list
 
-        # Splits the list into smaller lists, where the data is username and password
-        for i in str_lines:
-            line_lst.append(i.split(', '))
-        file.close()
-        return line_lst
+            # Splits the list into smaller lists, where the data is username and password
+            for i in str_lines:
+                line_lst.append(i.split(', '))
+            file.close()
+            return line_lst
+    except FileNotFoundError:
+        print('Unable to open file. It may not exist or it\'s corrupted.')
 
 
 def get_data(credential_file, _type=0):
@@ -33,3 +36,18 @@ def get_data(credential_file, _type=0):
     else:
         raise IndexError('Max length is 2')
     return passwords
+
+
+def get_line_count(credential_file):
+    """
+        :param credential_file: String containing the path plus filename which contains user + password data pattern
+        :return: Integer that represent the line count in the file
+    """
+    try:
+        with open(credential_file, 'r') as file:            
+            for index, line in enumerate(file):
+                pass
+        return index + 1
+    except FileNotFoundError:
+        print('Unable to open file. It may not exist or it\'s corrupted.')
+
