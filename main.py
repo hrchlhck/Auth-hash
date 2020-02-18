0a2bd4b8e0a3a720318a83bd77d480b84458de13
vpemfh7 <vpemfh7@gmail.com>
2020-02-18 09:04:51 -0300

    Added new option to brute force the passwords and did some code refactor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from py.data import DataReader
-import py.auth as auth
+from py.auth import sign_in, sign_up
+from py.brute_force import main as  bf_main
 
 def main():
     data = DataReader('files/credentials.txt')
@@ -7,19 +8,22 @@ def main():
     while True:
         print('(1) >> Sign up')
         print('(2) >> Log in')
-        print('(3) >> Exit')
+        print('(3) >> Crack passwords')
+        print('(4) >> Exit')
         option = int(input('>> '))
         
         credential_list = data.get_data()
         if option == 1:
             username = str(input('>> Type a username: '))
             password = str(input('>> Type a password: '))
-            auth.sign_up(username, password, credential_list)
+            sign_up(username, password, credential_list)
         elif option == 2:
             username = str(input('>> Type your username: '))
             password = str(input('>> Type your password: '))
-            auth.sign_in(username, password, credential_list)
+            sign_in(username, password, credential_list)
         elif option == 3:
+            bf_main()
+        elif option == 4:
             print('Good bye!')
             break
         else:
